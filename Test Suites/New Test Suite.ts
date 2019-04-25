672a7cdd2fbafd975684db7858502c8d58dadce6
Lieu Huynh <lieuhuynh@kms.com.vn>
2019-04-25 11:20:06 +0700

    'update'

---
+++ Test Suites/New Test Suite.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>fce24061-1eac-473c-b6b5-1bace6235fd1</testSuiteGuid>
+   <testCaseLink>
+      <guid>3bd99e86-9edb-4902-a6b7-7c88cf3fb4d8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/New Test Case</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
